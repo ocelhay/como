dfe8dcf352d02c6fc5249c2adb26b3b618421fdf
Ernest Guevarra <ernest@validmeasures.org>
2020-04-10 21:17:00 +0100

    add package documentation script

---
+++ R/como.R
@@ -0,0 +1,13 @@
+#' CoMo App
+#' 
+#' Developed by the Covid-19 International Modelling Consortium 
+#' (CoMo Consortium). This app allows to perform a model fit to the available 
+#' data and investigate the potential impact of several non-pharmaceutical 
+#' interventions.
+#'
+#' @docType package
+#' @name como
+#' @keywords internal
+#' @importFrom shiny runApp
+#' 
+"_PACKAGE"
