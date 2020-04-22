d7073d95f479db1430df8f81be011eb6e6c5c74b
Olivier <olivier.celhay@gmail.com>
2020-04-22 14:42:42 -0700

    Readd  class="btn btn-success"

--- inst/comoapp/app.R
+++ inst/comoapp/app.R
@@ -114,19 +114,19 @@ ui <- function(request) {
                                   fluidRow(
                                     column(6, 
                                            conditionalPanel("output.status_app_output == 'No Baseline' | output.status_app_output == 'Ok Baseline'",
-                                                            actionButton("run_baseline", "Run Baseline"))
+                                                            actionButton("run_baseline", "Run Baseline", class="btn btn-success"))
                                     ),
                                     column(6, 
                                            conditionalPanel("output.status_app_output == 'Ok Baseline'",
-                                                            actionButton("validate_baseline", span(icon("thumbs-up"), " Validate Baseline")),
+                                                            actionButton("validate_baseline", span(icon("thumbs-up"), " Validate Baseline"), class="btn btn-success"),
                                            )
                                     )
                                   ),
                                   conditionalPanel("(output.status_app_output == 'Validated Baseline' | output.status_app_output == 'Locked Baseline')  && input.tabs == 'tab_visualfit'", 
-                                                   actionButton("reset_baseline", span(icon("eraser"), "Reset the Baseline"))
+                                                   actionButton("reset_baseline", span(icon("eraser"), "Reset the Baseline"), class="btn btn-success")
                                   ),
                                   conditionalPanel("(output.status_app_output == 'Validated Baseline' | output.status_app_output == 'Locked Baseline') && input.tabs == 'tab_modelpredictions'",
-                                                   actionButton("run_interventions", "Run Future Scenarios")
+                                                   actionButton("run_interventions", "Run Future Scenarios", class="btn btn-success")
                                   ),
                               )
              )
