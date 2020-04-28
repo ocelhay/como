55907ebc92ccc7cabda2fa70bee7738441072a70
Olivier <olivier.celhay@gmail.com>
2020-04-28 14:48:40 -0700

    Focus on natural/COVID-19 deaths switch

--- inst/comoapp/app.R
+++ inst/comoapp/app.R
@@ -194,16 +194,26 @@ ui <- function(request) {
                                                                                       selected = "Predicted Reported + Unreported", inline = TRUE),
                                                                    fluidRow(
                                                                      column(6,
-                                                                            highchartOutput("highchart_cases_dual_baseline", height = "350px") %>% withSpinner(), br(),
+                                                                            highchartOutput("highchart_cases_dual_baseline", height = "350px") %>% withSpinner(), br()
+                                                                     ),
+                                                                     column(6,
+                                                                            highchartOutput("highchart_cases_dual_interventions", height = "350px") %>% withSpinner(), br()
+                                                                     )
+                                                                   ),
+                                                                   prettyRadioButtons("focus_natural_death", label = "Focus on:", 
+                                                                                      choices = c("No Focus", "COVID-19 Deaths"), 
+                                                                                      selected = "No Focus", inline = TRUE),
+                                                                   fluidRow(
+                                                                     column(6,
                                                                             highchartOutput("highchart_deaths_dual_baseline", height = "350px") %>% withSpinner(), br(),
                                                                             plotOutput("plot_deaths_age_baseline") %>% withSpinner(), br()
                                                                      ),
                                                                      column(6,
-                                                                            highchartOutput("highchart_cases_dual_interventions", height = "350px") %>% withSpinner(), br(),
                                                                             highchartOutput("highchart_deaths_dual_interventions", height = "350px") %>% withSpinner(), br(),
                                                                             plotOutput("plot_deaths_age_interventions") %>% withSpinner(), br()
                                                                      )
                                                                    ),
+                                                                   
                                                                    prettyRadioButtons("focus_requirements", label = "Focus on:", 
                                                                                       choices = c("No Focus", "Hospital Beds", "ICU Beds", "Ventilators"), 
                                                                                       selected = "No Focus", inline = TRUE),
