afae0ba1679e548bb7236b2234d78bf5ffb7aba6
Olivier <olivier.celhay@gmail.com>
2020-04-29 11:01:56 -0700

    Correct subtitle

--- inst/comoapp/www/outputs/plot_mortality_lag_dual.R
+++ inst/comoapp/www/outputs/plot_mortality_lag_dual.R
@@ -23,7 +23,7 @@ output$plot_mortality_lag_baseline <- renderPlot({
     theme_minimal(base_size = 14) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1.2),
           legend.position = "bottom", legend.text = element_text(size = 13)) +
-    labs(title = "Case Fatality Rate by Age", subtitle = "Future Scenarios",
+    labs(title = "Case Fatality Rate by Age", subtitle = "Baseline",
          x = "", y = "Fatality Rate", color = "Measured at:")
 })
 
