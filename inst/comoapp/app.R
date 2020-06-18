651f989b9e8307e8e47b52103d21c6fef9fe940a
Olivier <olivier.celhay@gmail.com>
2020-06-18 14:58:56 -0700

    working version of v14.1.1

--- inst/comoapp/app.R
+++ inst/comoapp/app.R
@@ -630,18 +630,29 @@ server <- function(input, output, session) {
     simul_interventions$results <- NULL
     
     source("./www/model.R", local = TRUE)
+    source("./www/fun_single_run.R", local = TRUE)  # TODO: make it a real function and move this to the top of the App
     source("./www/fun_multi_runs.R", local = TRUE)  # TODO: make it a real function and move this to the top of the App
     
     vectors <- inputs(inp, 'Baseline (Calibration)', times = times, stopdate = stopdate)
     
     if(input$iterations == 1) {
-      simul_baseline$results <- multi_runs(Y, times, parameters, input = vectors, A = A)
+      simul_baseline$results <- single_run(Y, times, parameters, input = vectors, A = A)
     }
     
     if(input$iterations > 1) {
-      out <- multi_runs(Y, times, parameters, input = vectors, A = A)
-      simul_baseline$results <- process_ode_outcome(out, parameters, startdate, times, ihr, ifr, mort, popstruc, nature = "median")
-      # decide on what to do with those (naming...)
+      results <- multi_runs(Y, times, parameters, input = vectors, A = A)
+      # example where the criteria to sort runs is the value of "attributable_deaths_end"
+      
+      multi_attributable_deaths_end <- NULL
+      for (i in 1:input$iterations) {
+        multi_attributable_deaths_end <- c(multi_attributable_deaths_end, results[[i]]$attributable_deaths_end)
+      }
+      
+      index_min <- which.min(abs(multi_attributable_deaths_end - quantile(multi_attributable_deaths_end, parameters["confidence"])))
+      index_med <- which.min(abs(multi_attributable_deaths_end - quantile(multi_attributable_deaths_end, 0.5)))
+      index_max <- which.min(abs(multi_attributable_deaths_end - quantile(multi_attributable_deaths_end, (1 - parameters["confidence"]))))
+      
+      simul_baseline$results <- results[[index_med]]
     }
     simul_baseline$baseline_available <- TRUE
     
--- inst/comoapp/www/fun_multi_runs.R
+++ inst/comoapp/www/fun_multi_runs.R
@@ -1,10 +1,13 @@
 multi_runs <- function(Y, times, parameters, input, A){
   results <- list()
-  nrow <- length(times)
-  ncol <- (22*A) + 1
   
-  # case when one iteration
-  if(parameters["iterations"] == 1){
+  for (i in 1:parameters["iterations"]){
+    print(paste0("Iteration ", i))
+    
+    # add noise to parameters
+    parameters[parameters_noise] <- parameters[parameters_noise] + 
+      rnorm(length(parameters_noise), mean = 0, sd = parameters["noise"]*abs(parameters[parameters_noise]))
+    
     covidOdeCpp_reset()
     output_ode <- ode(y = Y, times = times, method = "euler", hini = 0.05, func = covidOdeCpp, 
                       parms = parameters, input = input, A = A,
@@ -13,35 +16,7 @@ multi_runs <- function(Y, times, parameters, input, A){
                       popbirth_col2=popbirth[,2], popstruc_col2=popstruc[,2],
                       ageing=ageing, ifr_col2=ifr[,2], ihr_col2=ihr[,2], mort_col=mort)
     
-    results <- process_ode_outcome(output_ode, parameters, startdate, times, ihr, ifr, mort, popstruc)
+    results[[i]] <- process_ode_outcome(output_ode, parameters, startdate, times, ihr, ifr, mort, popstruc)
   }
-  
-  # case when several iterations
-  if(parameters["iterations"] > 1){
-    multiple_output_ode <- NULL
-    
-    for (i in 1:parameters["iterations"]){
-      print(paste0("Iteration: ", i))
-      
-      # add noise to parameters
-      parameters[parameters_noise] <- parameters[parameters_noise] + 
-        rnorm(length(parameters_noise), mean = 0, sd = parameters["noise"]*abs(parameters[parameters_noise]))
-      
-      covidOdeCpp_reset()
-      output_ode <- ode(y = Y, times = times, method = "euler", hini = 0.05, func = covidOdeCpp, 
-                        parms = parameters, input = input, A = A,
-                        contact_home=contact_home, contact_school=contact_school,
-                        contact_work=contact_work, contact_other=contact_other,
-                        popbirth_col2=popbirth[,2], popstruc_col2=popstruc[,2],
-                        ageing=ageing, ifr_col2=ifr[,2], ihr_col2=ihr[,2], mort_col=mort)
-      
-      multiple_output_ode <- cbind(multiple_output_ode, output_ode %>% as.numeric())
-    }
-    
-    results$median <- apply(multiple_output_ode, 1, quantile, probs = 0.5) %>% matrix(nrow = nrow, ncol = ncol)
-    results$min <- apply(multiple_output_ode, 1, quantile, probs = parameters["confidence"]) %>% matrix(nrow = nrow, ncol = ncol)
-    results$max <- apply(multiple_output_ode, 1, quantile, probs = (1 - parameters["confidence"])) %>% matrix(nrow = nrow, ncol = ncol)
-  }
-  
   return(results)
 }
