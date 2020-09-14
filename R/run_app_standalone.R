6bd627696d18237a5733d48077a3b29fe666212f
Olivier <olivier.celhay@gmail.com>
2020-09-14 15:36:46 -0400

    prepare standalone

---
+++ R/run_app_standalone.R
@@ -0,0 +1,14 @@
+#' Run the standalone version
+#'
+#' @return
+#' Open browser
+#' @export
+#'
+#' @import bsplus deSolve gridExtra highcharter knitr lubridate pushbar RColorBrewer Rcpp readxl 
+#' @import reshape2 scales shiny shinycssloaders shinyhelper shinyjs shinythemes 
+#' @import shinyWidgets tidyverse
+
+run_app_standalone <- function(options = list()) {
+  app_dir <- system.file("comoapp", package = "como")
+  shinyAppDir(app_dir, options = options)
+}
