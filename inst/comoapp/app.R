410626c6e13068af64f2214c997302f7007e13b1
Olivier <olivier.celhay@gmail.com>
2020-09-29 15:48:20 -0400

    add diagnosis

--- inst/comoapp/app.R
+++ inst/comoapp/app.R
@@ -51,7 +51,7 @@ ui <- function(request) {
       tabPanel("Welcome", value = "tab_welcome",
                h4(paste0("App ", version_app)),
                # for debugging purposes, TODO: remove in prod
-               # htmlOutput("diagnosis_platform"),
+               htmlOutput("diagnosis_platform"),
                
                fluidRow(
                  column(6,
@@ -323,11 +323,11 @@ ui <- function(request) {
 server <- function(input, output, session) {
   
   # for debugging purposes, TODO: remove in prod
-  # output$diagnosis_platform <- renderText({
-  #   paste0("pandoc_available: ", pandoc_available(), "</br>",
-  #          "Sys.getenv('PATH'): ", Sys.getenv("PATH"), "</br>",
-  #          "find_pandoc(dir = '/usr/local/bin/')", find_pandoc(dir = "/usr/local/bin/")$version)
-  # })
+  output$diagnosis_platform <- renderText({
+    paste0("pandoc_available: ", pandoc_available(), "</br>",
+           "Sys.getenv('PATH'): ", Sys.getenv("PATH"), "</br>",
+           "find_pandoc(dir = '/usr/local/bin/')", find_pandoc(dir = "/usr/local/bin/")$version)
+  })
   
   # triggers the modal dialogs when the user clicks an icon
   observe_helpers(help_dir = "./www/markdown")
