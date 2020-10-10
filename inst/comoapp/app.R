5e4ec59c077e010beced51c37bf5b16d66ea4b10
Olivier <olivier.celhay@gmail.com>
2020-10-10 14:01:35 -0400

    version 16.2.1

--- inst/comoapp/app.R
+++ inst/comoapp/app.R
@@ -1,5 +1,5 @@
 # CoMo COVID-19 App
-version_app <- "v16.3.0"
+version_app <- "v16.2.1"
 
 # To generate report with macOS standalone app (shinybox),
 # ensure that the R session has access to pandoc installed in "/usr/local/bin".
