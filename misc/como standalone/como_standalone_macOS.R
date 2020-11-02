b477b38710c0e0e2fb3455f3a26da0db1421d2cd
Olivier <olivier.celhay@gmail.com>
2020-11-02 09:59:45 -0500

    Update como_standalone_macOS.R

--- misc/como standalone/como_standalone_macOS.R
+++ misc/como standalone/como_standalone_macOS.R
@@ -1,44 +1,45 @@
 # Perform (once per machine) a manual installation of nodejs (https://nodejs.org/en/).
 
 # Install latest version of shinybox
-# remove.packages("shinybox")
-# detach("package:shinybox", unload = TRUE)
-# remotes::install_github("ocelhay/shinybox", auth_token = "")
-
 rm(list = ls())
-.rs.restartR()
+remove.packages("shinybox")
+detach("package:shinybox", unload = TRUE)
+remotes::install_github("ocelhay/shinybox", auth_token = "82b805c2e3d9032d569b228c4f9507f6d13bdd4c")
 library(shinybox)
 
 # Check that the package is working.
 # remotes::install_github("ocelhay/como", ref = "dev")
 # como::run_app_standalone()
 
-# Build the path
+# Build a directory on Dektop
 time <- format(Sys.time(), "%Y-%m-%d_%H%M%S")
 (build_path <- paste0("/Users/olivier/Desktop/", time))
 dir.create(build_path)
 
+
+
 nodejs_path <- "/usr/local/bin/"
 nodejs_version <- system("node -v", intern = TRUE)
 
-
-electrify(
+shinybox(
   app_name = "CoMo",
-  short_description = "CoMo Consortium | COVID-19 App",
-  semantic_version = "16.2.4", 
-  build_path = build_path,
+  author = "CoMo Consortium",
+  description = "Model from the Covid-19 International Modelling Consortium",
+  semantic_version = "v16.2.5",
   mran_date = "2020-09-30",
-  # cran_like_url = "https://cran.r-project.org/",
-  function_name = "run_app_standalone",
+  cran_like_url = NULL,
+  mac_url = "https://mac.r-project.org/high-sierra/R-4.0-branch/x86_64/R-4.0-branch.tar.gz",
   git_host = "github",
   git_repo = "ocelhay/como@dev",
+  function_name = "run_app_standalone", 
   local_package_path = NULL,
   package_install_opts = list(type = "binary"),
+  build_path = build_path,
   rtools_path_win = NULL,
-  run_build = TRUE,
+  nodejs_path = file.path(system.file(package = "shinybox"), "nodejs"),
   nodejs_version = nodejs_version,
-  mac_url = "https://mac.r-project.org/high-sierra/R-4.0-branch/x86_64/R-4.0-branch.tar.gz",
-  permission = TRUE)
+  permission = TRUE,
+  run_build = TRUE)
 
 
 # run_build_release(nodejs_path = nodejs_path,
