ae7b6cd801d68503d99a38cdd2aacd88a7a242a6
Olivier <olivier.celhay@gmail.com>
2020-11-18 16:38:41 -0500

    change default values

--- inst/comoapp/www/ui/pushbar_parameters_reporting.R
+++ inst/comoapp/www/ui/pushbar_parameters_reporting.R
@@ -3,9 +3,9 @@ list(
           br(), br(), br(),
           fluidRow(
             column(6,
-                   numericInput("init", label = "Density of exposed people at start date (expressed as 1/X)", value = 5000000, min = 1, max = NA, width = "50%"),
+                   numericInput("init", label = "Number of exposed people at start date ", value = 10, min = 1, max = NA, width = "50%"),
                    sliderInput("pre", label = "Proportion of population with partial immunity at the start date", value = 0, min = 0, max = 100, step = 0.1, post = "%", ticks = FALSE),
-                   sliderInput("reporth_g", label = "Percentage of denied hospitalisations that are reported", value = 10, min = 0, max = 100, step = 0.1, post = "%", ticks = FALSE, width = "75%"),
+                   sliderInput("reporth_g", label = "Percentage of denied hospitalisations that are reported", value = 90, min = 0, max = 100, step = 0.1, post = "%", ticks = FALSE, width = "75%"),
                    
                    sliderInput("reporth", label = "Percentage of non-severe hospitalisations that are appropriately treated", value = 90, min = 0, max = 100, step = 0.1, post = "%", ticks = FALSE, width = "75%"),
                    sliderInput("reporth_ICU", label = "Percentage of severe hospitalisations that are appropriately treated", value = 90, min = 0, max = 100, step = 0.1, post = "%", ticks = FALSE, width = "75%"),
@@ -18,7 +18,7 @@ list(
                    sliderInput("report_cvr", label = " Percentage of all symptomatic infections in previously vaccinated and exposed people that are reported", value = 0, min = 0, max = 100, step = 0.1, post = "%", ticks = FALSE, width = "75%"),
                    
                    sliderInput("report_cr", label = "Percentage of all symptomatic infections in previously infected people that are reported", value = 0, min = 0, max = 100, step = 0.1, post = "%", ticks = FALSE, width = "75%"),
-                   sliderInput("report_natdeathI", label = "Percentage of all people dying outside the hospital with asymptomatic infections reported as covid-deaths", value = 5, min = 0, max = 100, step = 0.1, post = "%", ticks = FALSE, width = "75%"),
+                   sliderInput("report_natdeathI", label = "Percentage of all people dying outside the hospital with asymptomatic infections reported as covid-deaths", value = 0, min = 0, max = 100, step = 0.1, post = "%", ticks = FALSE, width = "75%"),
                    sliderInput("report_natdeathCL", label = "Percentage of all people dying outside the hospital with symptomatic infections reported as covid-deaths", value = 10, min = 0, max = 100, step = 0.1, post = "%", ticks = FALSE, width = "75%"),
                    sliderInput("report_death_HC", label = "Percentage of all people dying outside the hospital with severe infections reported as covid-deaths", value = 10, min = 0, max = 100, step = 0.1, post = "%", ticks = FALSE, width = "75%")
             )
