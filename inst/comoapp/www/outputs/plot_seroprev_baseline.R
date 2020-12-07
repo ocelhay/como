e3c7b64bd5cd6517e9ae47e36962f309fb4cecb9
Olivier <olivier.celhay@gmail.com>
2020-12-07 15:39:24 -0500

    Update plot_seroprev_baseline.R

--- inst/comoapp/www/outputs/plot_seroprev_baseline.R
+++ inst/comoapp/www/outputs/plot_seroprev_baseline.R
@@ -10,28 +10,30 @@ output$plot_seroprev_baseline <- renderPlot({
                             input$sample_size,
                             input$sample_size / 5))
   
-  ab <- data.frame(Time = rep(simul_baseline$results$time, 1000), 
+  ab <- data.frame(Time = rep(simul_baseline$results$time, 100), 
                    Ab = 0)
   
   aux <- NULL
-  for (i in 1:1000) {
+  for (i in 1:100) {
     num.inf.samp <- rbinom(length(simul_baseline$results$time), size = samp.sizes, 
                            prob = (simul_baseline$results$med$ab_all_ages / simul_baseline$results$med$N))
     aux<-c(aux, num.inf.samp / samp.sizes)
   }
   
-  se<-0.5
-  sp<-0.9
+  se<-0.75
+  sp<-0.97
   ab$Ab<-se*aux+(1-sp)*(1-aux)
   
   ab %>%
-    filter(wday(Time) == 1) %>%
+    filter(mday(Time) %in% c(1, 15)) %>%
     filter(Time <= max_x) %>%
     mutate(Ab = 100 * Ab) %>%
   ggplot(aes(x = Time, y = Ab, group = Time)) + 
     coord_cartesian(ylim = c(0, 100)) +
     geom_boxplot() +
     scale_x_date(date_labels =  "%b %Y") +
-    labs(title = "Seroprevalence - PROVISIONAL PLOT", x= "", y = "%") +
+    labs(title = "Seroprevalence", 
+         subtitle = "The plot shows the expected seroprevalence at predetermined times\nif we were to sample a user-defined number of individuals and perform an antibody test on them.\nThe prevalence levels shown are corrected for test sensitivity/specificity (resp. 75/97%).", 
+         x= "", y = "%") +
     theme_light(base_size = 14)
 })
