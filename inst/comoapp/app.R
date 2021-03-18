84ad600f9974cbca0d85498342b0c68f5c495db1
Olivier <olivier.celhay@gmail.com>
2021-03-17 20:53:02 -0400

    versioning 18.0.1

--- inst/comoapp/app.R
+++ inst/comoapp/app.R
@@ -1,5 +1,5 @@
 # CoMo COVID-19 App
-version_app <- "v18.0.0"
+version_app <- "v18.0.1"
 
 # To generate report with macOS standalone app (created with shinybox),
 # ensure that the R session has access to pandoc installed in "/usr/local/bin".
--- misc/como standalone/como_standalone_macOS.R
+++ misc/como standalone/como_standalone_macOS.R
@@ -5,46 +5,34 @@ if(FALSE){
   rm(list = ls())
   remove.packages("shinybox")
   detach("package:shinybox", unload = TRUE)
-  remotes::install_github("ocelhay/shinybox", auth_token = "")
+  remotes::install_github("ocelhay/shinybox")
 }
 
 library(shinybox)
 
 # Check that the package is working.
-# remotes::install_github("ocelhay/como", ref = "dev")
+# remotes::install_github("ocelhay/como", ref = "v18")
 # como::run_app_standalone()
 
-# Build a directory on Dektop
 time <- format(Sys.time(), "%Y-%m-%d_%H%M%S")
 (build_path <- paste0("/Users/olivier/Documents/Projets/CoMo/Standalone_", time))
 dir.create(build_path)
 
 
-
-nodejs_path <- "/usr/local/bin/"
-nodejs_version <- system("node -v", intern = TRUE)
-
 shinybox(
   app_name = "CoMo",
   author = "CoMo Consortium",
   description = "Model from the Covid-19 International Modelling Consortium",
-  semantic_version = "v18.0.0", # important de garder le format vx.y.z - to add as a validation?
-  mran_date = "2020-12-01",
-  cran_like_url = NULL,
-  mac_url = "https://mac.r-project.org/high-sierra/R-4.0-branch/x86_64/R-4.0-branch.tar.gz",
+  semantic_version = "v18.0.1", # important de garder le format vx.y.z - to add as a validation?
+  cran_like_url = "https://cran.microsoft.com/snapshot/2021-01-10",
+  mac_file = "/Users/olivier/Documents/Projets/Standalone R Shiny/R/macOS/2020-10-13/R-4.0-branch.tar.gz",
+  mac_r_url = "https://mac.r-project.org/high-sierra/R-4.0-branch/x86_64/R-4.0-branch.tar.gz", # only used if mac_file is NULL
   git_host = "github",
-  git_repo = "ocelhay/como@master",
+  git_repo = "ocelhay/como@v18",
   function_name = "run_app_standalone", 
   local_package_path = NULL,
   package_install_opts = list(type = "binary"),
   build_path = build_path,
   rtools_path_win = NULL,
-  nodejs_path = file.path(system.file(package = "shinybox"), "nodejs"),
-  nodejs_version = nodejs_version,
-  permission = TRUE,
+  nodejs_path = "/usr/local/bin/",
   run_build = TRUE)
-
-
-# run_build_release(nodejs_path = nodejs_path,
-#                   app_path = paste0(build_path, "/CoMo"),
-#                   nodejs_version = nodejs_version)
